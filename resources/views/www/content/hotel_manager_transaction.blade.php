6acaad54cb32cbdeca6f9b54cfe4f1396e318ed1
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-01-11 09:49:48 +0100

    add fields

--- resources/views/www/content/hotel_manager_transaction.blade.php
+++ resources/views/www/content/hotel_manager_transaction.blade.php
@@ -12,23 +12,31 @@
         {{ csrf_field() }}
         <input type="hidden" name="transaction" value="{{ $transaction->id }}">
         <div class="form-group">
-            <label for="inputLang">Idioma</label>
+            <label for="inputLang">Idioma *</label>
             <input type="text" class="form-control" id="inputLang" name="lang" value="es">
         </div>
         <div class="form-group">
-            <label for="inputCheckInDate">CheckIn Date</label>
+            <label for="inputCheckInDate">CheckIn Date *</label>
             <input type="text" class="form-control" id="inputCheckInDate" name="checkInDate" value="{{ $checkInDate }}">
         </div>
         <div class="form-group">
-            <label for="inputCheckOutDate">CheckOut Date</label>
+            <label for="inputCheckOutDate">CheckOut Date *</label>
             <input type="text" class="form-control" id="inputCheckOutDate" name="checkOutDate" value="{{ $checkOutDate }}">
         </div>
         <div class="form-group">
-            <label for="inputNumberRooms">Número de habitaciones</label>
+            <label for="inputCheckInHour">Hora llegada</label>
+            <input type="text" class="form-control" id="inputCheckInHour" name="checkInHour" value="">
+        </div>
+        <div class="form-group">
+            <label for="inputCheckInMinute">Minuto llegada</label>
+            <input type="text" class="form-control" id="inputCheckInMinute" name="checkInMinute" value="">
+        </div>
+        <div class="form-group">
+            <label for="inputNumberRooms">Número de habitaciones *</label>
             <input type="number" class="form-control" id="inputNumberRooms" name="numberRooms" value="{{ $numberRooms }}">
         </div>
         <div class="form-group">
-            <label for="inputNumberAdults">Número de adultos</label>
+            <label for="inputNumberAdults">Número de adultos *</label>
             <input type="number" class="form-control" id="inputNumberAdults" name="numberAdults" value="{{ $numberAdults }}">
         </div>
         <div class="form-group">
@@ -39,55 +47,66 @@
         <hr>
 
         <div class="form-group">
-            <label for="inputName">Nombre</label>
+            <label for="inputName">Nombre *</label>
             <input type="text" class="form-control" id="inputName" name="name" value="">
         </div>
         <div class="form-group">
-            <label for="inputSurname">Apellidos</label>
+            <label for="inputSurname">Apellidos *</label>
             <input type="text" class="form-control" id="inputSurname" name="surname" value="">
         </div>
         <div class="form-group">
-            <label for="inputPhone">Teléfono</label>
+            <label for="inputPhone">Teléfono *</label>
             <input type="text" class="form-control" id="inputPhone" name="phone" value="">
         </div>
         <div class="form-group">
-            <label for="inputEmail">Email</label>
+            <label for="inputEmail">Email *</label>
             <input type="text" class="form-control" id="inputEmail" name="email" value="">
         </div>
 
         <div class="form-group">
-            <label for="inputDocType">DocType</label>
-            <input type="text" class="form-control" id="inputDocType" name="docType" value="">
+            <label for="inputDocType">DocType *</label>
+            <select class="form-control" id="inputDocType" name="docType">
+                <option value="1">DNI</option>
+                <option value="2">NIE</option>
+                <option value="3">Pasaporte</option>
+                <option value="4">CIF</option>
+            </select>
         </div>
         <div class="form-group">
-            <label for="inputDocNumber">DocNumber</label>
+            <label for="inputDocNumber">DocNumber *</label>
             <input type="text" class="form-control" id="inputDocNumber" name="docNumber" value="">
         </div>
         <div class="form-group">
             <label for="inputObservations">Observaciones</label>
-            <input type="text" class="form-control" id="inputObservations" name="observations" value="">
+            <textarea class="form-control" id="inputObservations" name="observations"></textarea>
         </div>
 
         <div class="form-group">
             <label for="inputCountry">País</label>
-            <input type="text" class="form-control" id="inputCountry" name="country" value="">
+            <select class="form-control" id="inputCountry" name="country">
+            </select>
         </div>
 
         <div class="form-group">
-            <label for="inputPayment">Medio de pago</label>
-            <input type="text" class="form-control" id="inputPayment" name="payment" value="">
+            <label for="inputPayment">Medio de pago *</label>
+            <select class="form-control" id="inputPayment" name="payment">
+                <option value="1">Visa</option>
+                <option value="2">American Express</option>
+                <option value="3">Master Card</option>
+                <option value="4">Depósito Bancario</option>
+            </select>
         </div>
         <div class="form-group">
-            <label for="inputCreditCardHolder">Titular Tarjeta de crédito</label>
+            <label for="inputCreditCardHolder">Titular Tarjeta de crédito *</label>
             <input type="text" class="form-control" id="inputCreditCardHolder" name="creditCardHolder" value="">
         </div>
         <div class="form-group">
-            <label for="inputCreditCard">Número Tarjeta de crédito</label>
+            <label for="inputCreditCard">Número Tarjeta de crédito *</label>
             <input type="text" class="form-control" id="inputCreditCard" name="creditCard" value="">
         </div>
         <div class="form-group">
-            <label for="inputDateExpiry">Fecha vencimiento</label>
-            <input type="text" class="form-control" id="inputDateExpiry" name="dateExpiry" value="">
+            <label for="inputDateExpiry">Fecha vencimiento *</label>
+            <input type="text" class="form-control" id="inputDateExpiry" name="dateExpiry" value="" placeholder="YYMM">
         </div>
         <div class="form-group">
             <label for="inputCvv">CVV</label>
